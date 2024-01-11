f75ba717d1766d2fd208f8e977197087a6e8763d
Rohit Kumar <97551067+rk6093720@users.noreply.github.com>
2024-01-11 17:12:31 +0530

    done

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -1,6 +1,6 @@
 const { UserModal } = require("../Modal/user.modal");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const secret = process.env.SECRET;
 const Register = async(req,res)=>{
     const {email,password}= req.body;
